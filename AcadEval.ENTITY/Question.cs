befa41cc760017721559bf80de8bd11f0dcb401d
Tiziano Mendez <166566359+Tmendezz@users.noreply.github.com>
2025-04-08 21:45:44 -0300

    feat: Entity Creation
    We created the principal entities for our project.

---
+++ AcadEval.ENTITY/Question.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcadEval.ENTITY
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int SurveyId { get; set; }
+
+        [ForeignKey("SurveyId")]
+        public virtual Survey Survey { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Text { get; set; } = string.Empty;
+
+        [Required]
+        public bool IsRequired { get; set; } = false;
+
+        [Required]
+        public QuestionType Type {  get; set; }
+
+        public virtual ICollection<Option> Options { get; set; }
+
+    }
+    public enum QuestionType
+    {
+        MultipleChoice = 1,
+        //Respuesta libre en texto
+        Open = 2,
+        //Escala numerica para calificar algo
+        NumericScale = 3
+    }
+
+}
