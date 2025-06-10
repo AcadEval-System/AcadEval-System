4f1cf7c4ab3a525a9807ac62d3ecd675793c0914
Tmendezz <Tmendezxx1@gmail.com>
2025-06-09 19:19:26 -0700

    test: CareerControllerTest para GetAll()

---
+++ tests/AcadEvalSys.API.Tests/Controllers/CareerControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+using FluentAssertions;
+
+namespace AcadEvalSys.API.Tests.Controllers
+{
+    public class CareerControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        public CareerControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAll_ForValidRequest_Return200Ok()
+        {
+            var client = _factory.CreateClient();
+
+            var result = await client.GetAsync("/careers?pageNumber=1&pageSize=10");
+
+            result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+            
+        }
+    }
+}
