883aa58e8d5d726e37b7770db63b277544779892
tomib4n <tbanchio15@gmail.com>
2025-06-23 15:13:21 -0300

    feat(competencias): añade descripción de niveles de competencia y actualiza mapeo en DTOs
    - Se agrega la clase CompetencyLevelDescriptionDto para representar la descripción de los niveles de competencia.
    - Se actualiza CompetencyDto para incluir una colección de niveles de competencia.
    - Se modifica el mapeo en CompetencyProfile para incluir los niveles de competencia desde LevelDescriptions.
    - Se mejora la consulta en CompetencyRepository para incluir descripciones de niveles al obtener competencias.

--- src/AcadEvalSys.Application/Competencies/Dtos/CompetencyDto.cs
+++ src/AcadEvalSys.Application/Competencies/Dtos/CompetencyDto.cs
@@ -8,4 +8,5 @@ public class CompetencyDto
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public CompetencyType Type { get; set; } 
+    public IReadOnlyCollection<CompetencyLevelDescriptionDto> Levels { get; set; } = [];
 }
